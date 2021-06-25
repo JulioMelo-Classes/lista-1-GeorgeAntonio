86ee32552993b5736d7cacc7c2c43df6b4bac7c8
george antonio dos santos bezerra <soriso_metalico2007@hotmail.com>
2021-06-25 15:20:27 -0300

    alteração do arq autor, somavizinhos, negativos, fibonacci

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -4,13 +4,30 @@
  * @data June, 6th 2021
  */
 #include <iostream>
+#include "math.h"
 using std::cout;
 using std::cin;
 using std::endl;
 
-int main( void )
-{
-    // TODO: Adicione seu código aqui. 
-
+int main() {
+    int n, m, cont = 0, som;
+    std::cin >> n;
+    std::cin >> m;
+    som = n;
+    while (cont < abs(m) - 1) {
+        if (m > 0) {
+            n++;
+            som += n;
+        }
+        else if (m < 0) {
+            n--;
+            som += n;
+        }
+        else if (m == 0) {
+            break;
+        }
+        cont++;
+    }
+    std::cout << som;
     return 0;
 }
